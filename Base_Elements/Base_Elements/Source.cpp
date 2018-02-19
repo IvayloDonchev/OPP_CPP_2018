31825ff730c9b51f18c3c4304543c6e86a9fb9cd
Ivaylo Donchev <idonchev@hotmail.com>
2018-02-19 09:30:43 +0200

    erg

---
+++ Base_Elements/Base_Elements/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 20;
+	int *ip = &i;
+	cout << "*ip = " << *ip << endl;
+	*ip = 50;
+	cout << "i = " << i << endl;
+	cout << "ip = " << ip << endl;
+	cout << "&i = " << &i << endl;
+
+	double d;
+	double *pd = &d;
+	char ch='A';
+	char *pch;
+	pch = &ch;
+
+	ip = reinterpret_cast<int*>(pch);
+	cout << "*ip = " << (char)(*ip) << endl;
+
+	i = (int)(&i);  // в променливата i е записан адреса на i
+	cout << hex << "i = " << i << endl;
+	ip = reinterpret_cast<int*>(i); // От число получаваме адрес. Сега ip отново сочи i
+	cout << "*ip = " << *ip << endl;
+
+	system("pause");
+
+
+
+
+}
