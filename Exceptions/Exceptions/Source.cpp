ed009f441a4c18252fcb95d70302a6c237c6b9df
Ivaylo Donchev <idonchev@hotmail.com>
2018-05-09 14:48:38 +0300

    дфг

--- Exceptions/Exceptions/Source.cpp
+++ Exceptions/Exceptions/Source.cpp
@@ -4,11 +4,16 @@
 #include <iostream>
 using namespace std;
 
+/*    Напишете метод ReadNumber(int start, int end), който въвежда от конзолата число 
+  в диапазона [start … end]. В случай на въведено невалидно число или число, което не е в подадения
+  диапазон, хвърлете подходящо изключение. Използвайки този метод, напишете програма, 
+  която въвежда 10 числа a1, a2, …, a10, такива, че 1 < a1 < … < a10 < 100.  */
+
 int ReadNumber(int start, int end)
 {
 	int x;
 	cout << "x = "; cin >> x;
-	if (x <= start || x > end)
+	if (x < start || x > end)
 		throw OutOfRangeException("Incorrect number");
 	return x;
 }
@@ -18,35 +23,26 @@ int main()
 	const int N = 10;
 	int mas[N];
 	int i = 0;
-	mas[0] = 0;
-	int x;
-	loop:
-	try {
-		x = ReadNumber(0, 100);
-	}
-	catch (OutOfRangeException &e)
-	{
-		cout << e.what();
-		goto loop;
-	}
-	mas[0] = x;
-	while (i < N)
-	{
-		cout << "Enter a number [" << i << "]: ";
+	int left = 1, right = 100;
+	do {
+		cout << "[" << i << "] Enter a number between " << left+1 << " and " << right-N+i << ": ";
 		try {
-			x = ReadNumber(mas[i-1], 100);
+			mas[i++] = ReadNumber(left+1, right-N+i);
+			left = mas[i - 1];
 		}
 		catch (OutOfRangeException &e)
 		{
 			cout << e.what() << endl;
-			continue;
 		}
-		mas[i++] = x;
-	}
-	for (int i = 0; i < N; i++)
+	} while (i < N);
+	for (int i = 0; i < N; i++)  // Извеждане на резултатите
 		cout << mas[i] << " ";
 	cout << endl;
 
+	/*   Напишете програма, която прочита от конзолата цяло положително число и отпечатва 
+	  на конзолата корен квадратен от това число. Ако числото е отрицателно или невалидно, 
+	  да се изпише "Invalid Number" на конзолата. Във всички случаи да се принтира на конзолата
+	  "Good Bye".	*/
 
 	/*int n;
 	cout << "n = "; cin >> n;
