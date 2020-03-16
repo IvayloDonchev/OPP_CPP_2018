d71d18960233a3104370348f796409c8070122f7
Ivaylo Donchev <idonchev@hotmail.com>
2020-03-16 20:34:28 +0200

    wed

--- KN_Move_Semantics/KN_Move_Semantics/Source.cpp
+++ KN_Move_Semantics/KN_Move_Semantics/Source.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 #include <cstring>
 using namespace std;
@@ -6,7 +6,7 @@ using namespace std;
 template <typename T>
 void Swap(T& a, T& b)
 {
-	T buf(move(a));	
+	T buf{move(a)};
 	a = (T&&) b;		
 	b = (T&&) buf;	
 }
@@ -18,7 +18,7 @@ public:
 		cout << this << " default constructor...\n";
 	}
 	mystring(const char* s) {
-		int sz = strlen(s) + 1;
+		size_t sz { strlen(s) + 1 };
 		buffer = new char[sz];
 		strcpy_s(buffer, sz, s);
 		cout << this << " constructor...\n";
@@ -27,9 +27,10 @@ public:
 		cout << this << " destructor...\n";
 		if(buffer) delete[] buffer;
 	}
+	//Copy semantics
 	mystring(const mystring& other) //copy constructor
 	{
-		int sz = strlen(other.buffer) + 1;
+		size_t sz{ strlen(other.buffer) + 1 };
 		buffer = new char[sz];
 		strcpy_s(buffer, sz, other.buffer);
 		cout << this << " copy constructor...\n";
@@ -38,20 +39,20 @@ public:
 	{
 		if (this == &other) return *this;
 		if (buffer) delete[] buffer;
-		int sz = strlen(other.buffer) + 1;
+		size_t sz{ strlen(other.buffer) + 1 };
 		buffer = new char[sz];
 		strcpy_s(buffer, sz, other.buffer);
 		cout << this << " copy assignment...\n";
 		return *this;
 	}
 	//Move semantics
-	mystring(mystring&& other) :	//move constructor
+	mystring(mystring&& other) noexcept :	//move constructor
 		buffer(move(other.buffer))
 	{
 		other.buffer = nullptr;
 		cout << this << " move constructor...\n";
 	}
-	mystring& operator=(mystring&& other)	//move assignment operator
+	mystring& operator=(mystring&& other) noexcept	//move assignment operator
 	{
 		if (this == &other) return *this;	//self assignment
 		if (buffer) delete[] buffer;
@@ -63,16 +64,18 @@ public:
 	void disp() const {
 		cout << buffer << endl;
 	}
+	friend ostream& operator << (ostream & out, const mystring & s) {
+		return out << s.buffer;
+	}
 };
-
 
 int main()
 {
 	mystring s1("Ivan Ivanov");   
-	mystring s2("Petar Jordanov");				  
+	mystring s2("Petar Petrov");				  
 	Swap(s1, s2);
-	cout << "s1: "; s1.disp(); cout << endl;
-	cout << "s2: "; s2.disp(); cout << endl;
+	cout << "s1: " << s1 << endl;
+	cout << "s2: " << s2 << endl;
 
 	system("pause");
 }
