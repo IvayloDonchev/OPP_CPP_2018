531bf7f660ceb73d2c783a30baa05de73a529c92
Ivaylo Donchev <idonchev@hotmail.com>
2020-03-16 21:06:26 +0200

    erd

--- KN_Move_Semantics/MoveSemanticsDemo/Source.cpp
+++ KN_Move_Semantics/MoveSemanticsDemo/Source.cpp
@@ -6,26 +6,30 @@ using namespace std;
 template <typename T>
 void Swap(T& a, T& b)
 {
-	T buf{move(a)};
-	a = (T&&) b;		
-	b = (T&&) buf;	
+	T buf{ move(a) };
+	a = (T&&)b;
+	b = move(buf);	
 }
 
 class mystring {
-	char *buffer;
+	char* buffer;
 public:
-	mystring() : buffer(nullptr) {
+	mystring() : buffer{ nullptr } 
+	{
 		cout << this << " default constructor...\n";
 	}
-	mystring(const char* s) {
-		size_t sz { strlen(s) + 1 };
+	mystring(const char* s)
+	{
+		size_t sz{ strlen(s) + 1 };
 		buffer = new char[sz];
 		strcpy_s(buffer, sz, s);
 		cout << this << " constructor...\n";
 	}
-	~mystring() {
+	~mystring()
+	{
 		cout << this << " destructor...\n";
-		if(buffer) delete[] buffer;
+		if (buffer)
+			delete[] buffer;
 	}
 	//Copy semantics
 	mystring(const mystring& other) //copy constructor
@@ -38,7 +42,8 @@ public:
 	mystring& operator=(const mystring& other)  //copy assignment
 	{
 		if (this == &other) return *this;
-		if (buffer) delete[] buffer;
+		if (buffer)
+			delete[] buffer;
 		size_t sz{ strlen(other.buffer) + 1 };
 		buffer = new char[sz];
 		strcpy_s(buffer, sz, other.buffer);
@@ -47,7 +52,7 @@ public:
 	}
 	//Move semantics
 	mystring(mystring&& other) noexcept :	//move constructor
-		buffer(move(other.buffer))
+		buffer{ move(other.buffer) }
 	{
 		other.buffer = nullptr;
 		cout << this << " move constructor...\n";
@@ -55,7 +60,8 @@ public:
 	mystring& operator=(mystring&& other) noexcept	//move assignment operator
 	{
 		if (this == &other) return *this;	//self assignment
-		if (buffer) delete[] buffer;
+		if (buffer)
+			delete[] buffer;
 		buffer = std::move(other.buffer);
 		other.buffer = nullptr;
 		cout << this << " move assignment...\n";
@@ -64,24 +70,19 @@ public:
 	void disp() const {
 		cout << buffer << endl;
 	}
-	friend ostream& operator << (ostream & out, const mystring & s) {
+	friend ostream& operator << (ostream& out, const mystring& s) {
 		return out << s.buffer;
 	}
 };
 
 int main()
 {
-	mystring s1("Ivan Ivanov");   
-	mystring s2("Petar Petrov");				  
+	mystring s1("Ivan Ivanov");
+	mystring s2("Petar Petrov");
+	cout << "---------------Swap----------------\n";
 	Swap(s1, s2);
+	cout << "---------------Swap----------------\n";
 	cout << "s1: " << s1 << endl;
 	cout << "s2: " << s2 << endl;
-
-	int a{ 1 }, b{ 2 }, c{ 3 }, d{ 4 };
-	tie(c, d) = tie(a, b);
-	cout << "a = " << a << endl;
-	cout << "b = " << b << endl;
-	cout << "c = " << c << endl;
-	cout << "d = " << d << endl;
 
 }
